6e70b5981bbab8a1ba13af15d665c3829faf9cb6
Markus Berg <thebergh@yahoo.com>
2017-04-27 21:05:07 +0800

    Created a resource file, and added an image. Created the basic design for the login page.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QStackedWidget>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //setCentralWidget(ui->stackedWidget); // do I need?
+
+    // set statusbar and main tool bar invisible for now while logging in
+    ui->statusBar->setVisible(false);
+    ui->mainToolBar->setVisible(false);
 }
 
 MainWindow::~MainWindow()
