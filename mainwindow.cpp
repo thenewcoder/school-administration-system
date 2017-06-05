63699cc9f1b45b796671a2bcb76fc6697ee22730
Markus Berg <thebergh@yahoo.com>
2017-06-05 22:49:31 +0800

    Added a member page for the stacked widget.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,11 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
-
-#include <QStackedWidget>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    //setCentralWidget(ui->stackedWidget); // do I need?
 
     // set statusbar and main tool bar invisible for now while logging in
     ui->statusBar->setVisible(false);
