f69bd49be2029db6f95dc7160c680c98c593a4ae
Markus Berg <thebergh@yahoo.com>
2017-07-13 23:06:50 +0800

    Added setters and getters to the student pagestudentsform.

--- pagestudentsform.cpp
+++ pagestudentsform.cpp
@@ -1,4 +1,4 @@
-#include "pagestudentsform.h"
+﻿#include "pagestudentsform.h"
 #include "ui_pagestudentsform.h"
 
 PageStudentsForm::PageStudentsForm(QWidget *parent) :
@@ -11,4 +11,124 @@ PageStudentsForm::PageStudentsForm(QWidget *parent) :
 PageStudentsForm::~PageStudentsForm()
 {
     delete ui;
+}
+
+QString PageStudentsForm::name() const
+{
+    return ui->leName->text();
+}
+
+void PageStudentsForm::setName(const QString &name)
+{
+    ui->leName->setText(name);
+}
+
+QDate PageStudentsForm::dateOfBirth() const
+{
+    return ui->deBirthDate->date();
+}
+
+void PageStudentsForm::setDateOfBirth(const QDate &date)
+{
+    ui->deBirthDate->setDate(date);
+}
+
+QString PageStudentsForm::gender() const
+{
+    return ui->cbGender->currentText();
+}
+
+void PageStudentsForm::setGender(const QString &gender)
+{
+    ui->cbGender->setCurrentText(gender);
+}
+
+QString PageStudentsForm::nationality() const
+{
+    return ui->cbNationality->currentText();
+}
+
+void PageStudentsForm::setNationality(const QString &nationality)
+{
+    ui->cbNationality->setCurrentText(nationality);
+}
+
+QString PageStudentsForm::passportNumber() const
+{
+    return ui->lePassportNumber->text();
+}
+
+void PageStudentsForm::setPassportNumber(const QString &number)
+{
+    ui->lePassportNumber->setText(number);
+}
+
+QString PageStudentsForm::idNumber() const
+{
+    return ui->leIDNumber->text();
+}
+
+void PageStudentsForm::setIdNumber(const QString &number)
+{
+    ui->leIDNumber->setText(number);
+}
+
+QString PageStudentsForm::address() const
+{
+    return ui->teAddress->toPlainText();
+}
+
+void PageStudentsForm::setAddress(const QString &address)
+{
+    ui->teAddress->setPlainText(address);
+}
+
+QString PageStudentsForm::studentPhoneNumber() const
+{
+    return ui->leStudentPhone->text();
+}
+
+void PageStudentsForm::setStudentPhoneNumber(const QString &number)
+{
+    ui->leStudentPhone->setText(number);
+}
+
+QString PageStudentsForm::studentEmail() const
+{
+    return ui->leStudentEmail->text();
+}
+
+void PageStudentsForm::setStudentEmail(const QString &email)
+{
+    ui->leStudentEmail->setText(email);
+}
+
+QString PageStudentsForm::fathersPhoneNumber() const
+{
+    return ui->leFathersPhone->text();
+}
+
+void PageStudentsForm::setFathersPhoneNumber(const QString &number)
+{
+    ui->leFathersPhone->setText(number);
+}
+
+QString PageStudentsForm::mothersPhoneNumber() const
+{
+    return ui->leMothersPhone->text();
+}
+
+void PageStudentsForm::setMothersPhoneNumber(const QString &number)
+{
+    ui->leMothersPhone->setText(number);
+}
+
+QString PageStudentsForm::parentEmail() const
+{
+    return ui->leParentEmail->text();
+}
+
+void PageStudentsForm::setParentEmail(const QString &email)
+{
+    ui->leParentEmail->setText(email);
 }
--- pagestudentsform.h
+++ pagestudentsform.h
@@ -1,4 +1,4 @@
-#ifndef PAGESTUDENTSFORM_H
+﻿#ifndef PAGESTUDENTSFORM_H
 #define PAGESTUDENTSFORM_H
 
 #include <QWidget>
@@ -14,6 +14,42 @@ class PageStudentsForm : public QWidget
 public:
     explicit PageStudentsForm(QWidget *parent = 0);
     ~PageStudentsForm();
+
+    QString name() const;
+    void setName(const QString &name);
+
+    QDate dateOfBirth() const;
+    void setDateOfBirth(const QDate &date);
+
+    QString gender() const;
+    void setGender(const QString &gender);
+
+    QString nationality() const;
+    void setNationality(const QString &nationality);
+
+    QString passportNumber() const;
+    void setPassportNumber(const QString &number);
+
+    QString idNumber() const;
+    void setIdNumber(const QString &number);
+
+    QString address() const;
+    void setAddress(const QString &address);
+
+    QString studentPhoneNumber() const;
+    void setStudentPhoneNumber(const QString &number);
+
+    QString studentEmail() const;
+    void setStudentEmail(const QString &email);
+
+    QString fathersPhoneNumber() const;
+    void setFathersPhoneNumber(const QString &number);
+
+    QString mothersPhoneNumber() const;
+    void setMothersPhoneNumber(const QString &number);
+
+    QString parentEmail() const;
+    void setParentEmail(const QString &email);
 
 private:
     Ui::PageStudentsForm *ui;
