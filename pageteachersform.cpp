776376558d8890d4368c44345709576e13a1a6bd
Markus Berg <thebergh@yahoo.com>
2017-07-13 10:09:28 +0800

    Added getters and setters to the PageTeachersForm class.

--- pageteachersform.cpp
+++ pageteachersform.cpp
@@ -12,3 +12,53 @@ PageTeachersForm::~PageTeachersForm()
 {
     delete ui;
 }
+
+QString PageTeachersForm::name() const
+{
+    return ui->leName->text();
+}
+
+void PageTeachersForm::setName(const QString &name)
+{
+    ui->leName->setText(name);
+}
+
+QString PageTeachersForm::gender() const
+{
+    return ui->cbGender->currentText();
+}
+
+void PageTeachersForm::setGender(const QString &gender)
+{
+    ui->cbGender->setCurrentText(gender);
+}
+
+QString PageTeachersForm::nationality() const
+{
+    return ui->cbNationality->currentText();
+}
+
+void PageTeachersForm::setNationality(const QString &nationality)
+{
+    ui->cbNationality->setCurrentText(nationality);
+}
+
+QString PageTeachersForm::address() const
+{
+    return ui->teAddress->toPlainText();
+}
+
+void PageTeachersForm::setAddress(const QString &address)
+{
+    ui->teAddress->setPlainText(address);
+}
+
+QString PageTeachersForm::phoneNumber() const
+{
+    return ui->lePhone->text();
+}
+
+void PageTeachersForm::setPhoneNumber(const QString &phoneNumber)
+{
+    ui->lePhone->setText(phoneNumber);
+}
--- pageteachersform.h
+++ pageteachersform.h
@@ -15,8 +15,24 @@ public:
     explicit PageTeachersForm(QWidget *parent = 0);
     ~PageTeachersForm();
 
+    QString name() const;
+    void setName(const QString &name);
+
+    QString gender() const;
+    void setGender(const QString &gender);
+
+    QString nationality() const;
+    void setNationality(const QString &nationality);
+
+    QString address() const;
+    void setAddress(const QString &address);
+
+    QString phoneNumber() const;
+    void setPhoneNumber(const QString &phoneNumber);
+
 private:
     Ui::PageTeachersForm *ui;
+
 };
 
 #endif // PAGETEACHERSFORM_H
